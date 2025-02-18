3beb0d3190ebf2fbbda5702743defbf0281f6e06
JGuzman87 <jguzman913@gmail.com>
2025-02-18 16:20:49 -0500

    added files

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import '../app/globals.css'
+import Blog from '../components/Blog';
+
+export default function Home() {
+  return (
+    <div className="comp-container">
+      <Blog />
+    </div>
+  );
+}
