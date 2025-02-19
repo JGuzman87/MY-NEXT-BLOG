20a016b5ef1fec7c3a196944b323580f0e5726da
JGuzman87 <jguzman913@gmail.com>
2025-02-18 21:54:06 -0500

    used state and modified css

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-import '../app/globals.css'
-import Form from '../components/Form';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
+import "../app/globals.css";
+import Form from "../components/Form";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <div className="comp-container">
-        <Form />
-      </div>
+
+      <Form />
+
       <Footer />
     </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,77 @@
-import '../app/globals.css'
+"use client"; 
+import { useState } from "react";
+import "../app/globals.css";
 
 const Form = () => {
+  const [userName, setUserName] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputName = target.name;
+    const inputValue = target.value
+    if(inputName === "userName"){
+      setUserName(inputValue);
+    } else if(inputName === "title") {
+      setTitle(inputValue);
+    } else if (inputName === "content") {
+      setContent(inputValue);
+    }
+      
+    
+    
+    console.log(inputName);
+  };
+  
+  
+  const handleClick = (e) => {
+
+    e.preventDefault();
+
+    setUserName('');
+    setTitle("");
+    setContent("");
+  };
+
   return (
     <form className="input-container">
       <label
-         htmlFor="username"
+        htmlFor="username"
         className="block text-sm/6 font-medium text-gray-900"
       >
         Username:
       </label>
       <input
+        value={userName}
         className="user-name"
-        name="unsername "
+        name="userName"
         type="text"
-        placeholder="username"
+        onChange={handleInputChange}
+        placeholder="Username"
       />
-      <label htmlFor="title" className="block text-sm/6 font-medium text-gray-900">
+      <label
+        htmlFor="title"
+        className="block text-sm/6 font-large text-gray-900"
+      >
         Title:
       </label>
-      <input className="title" name="title " type="title" placeholder="Title" />
-      <label  htmlFor="title" className="block text-sm/6 font-medium text-gray-900">
+      <input value={title} className="title" name="title" type="text" onChange={handleInputChange} placeholder="Title" />
+      <label
+        htmlFor="content"
+        className="block text-sm/6 font-medium text-gray-900"
+      >
         Content:
       </label>
-      <textarea className="content" name="content " type="content" placeholder="content"></textarea>
-      <button className="submit">Submit</button>
+      <textarea
+        value={content}
+        className="content"
+        name="content"
+        type="textarea"
+        onChange={handleInputChange}
+        placeholder="Content"
+      ></textarea>
+      <button className="submit" onClick={handleClick} >Submit</button>
     </form>
   );
 };
